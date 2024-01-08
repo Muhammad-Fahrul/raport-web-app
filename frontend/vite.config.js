4ce837c7c60010f091fc29646c6c3da0b64cd580
Muhammad-Fahrul <fahruldev22@gmail.com>
2024-01-08 21:20:13 +0800

    update vite config target url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://clean-cyan-tiara.cyclic.app",
+        target: "",
         changeOrigin: true,
       },
     },
